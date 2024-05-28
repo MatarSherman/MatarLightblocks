5f359868fc978212e8a3a7f165050e0adaac9097
MatarSherman <matarsherman1@email.com>
2024-05-28 13:44:19 +0300

    infnitescroll

--- src/app/team/components/TeamMembers/TeamMembers.jsx
+++ src/app/team/components/TeamMembers/TeamMembers.jsx
@@ -3,25 +3,45 @@
 import { useState } from "react";
 import { TeamMemberCard } from "./TeamMemberCard/TeamMemberCard";
 import { teamData } from "./teamData";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const size = 1;
 
 export function TeamMembersListMobile() {
   const [selectedMember, setSelectedMember] = useState(null);
-  const rows = chunkArray(teamData, 2);
+
+  const rows = chunkArray(teamData, 3);
+
+  const [pagination, setPagination] = useState(1)
+  const [team, setTeam] = useState([...rows.slice(0, pagination * size)])
+
+  const loadMoreTeam = () => {
+    // await new Promise(resolve => setTimeout(resolve, 300))
+    setTeam([...rows.slice(0, (pagination + 1) * size)]);
+    setPagination(curr => curr + 1);
+  };
+
   return (
     <div
       className="team-members-section--mobile"
       onClick={() => setSelectedMember(null)}
     >
-      {rows.map((row, i) => (
-        <TeamMembersRow
-          selectedMember={selectedMember}
-          setSelectedMember={setSelectedMember}
-          key={`team-member__row-${i}`}
-          members={row}
-          index={i}
-          isMobile
-        />
-      ))}
+      <InfiniteScroll
+        dataLength={team.length}
+        hasMore={rows.length > team.length}
+        next={loadMoreTeam}
+        style={{ width: "100%", overflow: "hidden" }}>
+        {team.map((row, i) => (
+          <TeamMembersRow
+            selectedMember={selectedMember}
+            setSelectedMember={setSelectedMember}
+            key={`team-member__row-${i}`}
+            members={row}
+            index={i}
+            isMobile
+          />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 }
@@ -29,17 +49,32 @@ export function TeamMembersListMobile() {
 export function TeamMembersListDesktop() {
   const rows = chunkArray(teamData, 3);
 
+  const [pagination, setPagination] = useState(1)
+  const [team, setTeam] = useState([...rows.slice(0, pagination * size)])
+
+  const loadMoreTeam = () => {
+    // await new Promise(resolve => setTimeout(resolve, 300))
+    setTeam([...rows.slice(0, (pagination + 1) * size)]);
+    setPagination(curr => curr + 1);
+  };
+
   return (
     <div className="team-members-section--desktop">
-      {rows.map((row, i) => {
-        return (
-          <TeamMembersRow
-            key={`team-member__row-${i}`}
-            members={row}
-            index={i}
-          />
-        );
-      })}
+      <InfiniteScroll
+        dataLength={team.length}
+        hasMore={rows.length > team.length}
+        next={loadMoreTeam}
+        style={{ width: "100%", overflow: "hidden" }}>
+        {team.map((row, i) => {
+          return (
+            <TeamMembersRow
+              key={`team-member__row-${i}`}
+              members={row}
+              index={i}
+            />
+          );
+        })}
+      </InfiniteScroll>
     </div>
   );
 }
@@ -73,9 +108,8 @@ function chunkArray(array, chunkSize) {
 export function TeamMembers({ isLoaded }) {
   return (
     <div
-      className={`team-members-section translate-container ${
-        !isLoaded ? "team-members-section--hidden" : ""
-      }`}
+      className={`team-members-section translate-container ${!isLoaded ? "team-members-section--hidden" : ""
+        }`}
     >
       <h2 className="team-members__heading">Meet the Team</h2>
       <TeamMembersListMobile />
